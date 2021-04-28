243ce849afb8bf246c8bf80240c9a6d16bd0fe5c
Samuel Smith <samuel.smith@se.com>
2021-04-28 14:53:18 +0200

    Yaml file stuff

--- lisp/lang-yaml.el
+++ lisp/lang-yaml.el
@@ -1,4 +1,7 @@
 (use-package yaml-mode
-  :ensure t)
+  :ensure t
+  :config
+  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+  (add-to-list 'auto-mode-alist '("\\.yaml\\'" . yaml-mode)))
 
 (provide 'lang-yaml)
