6b789dbdaf50a25eaa003222e646828571405bea
Samuel Smith <samuel.smith@se.com>
2023-06-09 13:01:53 +0200

    FIx php config

--- lisp/lang-php.el
+++ lisp/lang-php.el
@@ -1,7 +1,10 @@
 (use-package php-mode
  :ensure t
  :mode
- ("\\.php\\'" . php-mode))
+ ("\\.php\\'" . php-mode)
+ :config
+   (with-eval-after-load "lsp-mode"
+    (add-to-list 'lsp-enabled-clients 'iph)))
 
 (add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
 
