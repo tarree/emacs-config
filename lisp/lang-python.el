bb7da8d4742d4bccb9b9edd30700696958215ad4
Samuel Smith <samuel.smith@se.com>
2023-09-11 15:36:48 +0200

    Fix python to use jedi for ls

--- lisp/lang-python.el
+++ lisp/lang-python.el
@@ -18,7 +18,8 @@
   :ensure t
   :config
   (with-eval-after-load "lsp-mode"
-    (add-to-list 'lsp-disabled-clients 'pyls)))
+    (add-to-list 'lsp-disabled-clients 'pyls)
+    (add-to-list 'lsp-enabled-clients 'jedi)))
 
 
 (use-package pip-requirements
